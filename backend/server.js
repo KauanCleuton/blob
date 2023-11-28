159e97180bebf4ad3233ece87d8484d387580738
kauanbrtech <kauandasilva@brtech.dev>
2023-11-28 15:35:41 -0300

    Atualização

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
-const cors = require('cors');
+const cors = require('cors')
 const multer = require('multer');
-const path = require('path');
 
 const prisma = new PrismaClient();
 const app = express();
